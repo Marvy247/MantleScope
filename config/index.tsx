958d8e6568595ad764aabe087397be3a8b0fc6f1
Marvy247 <marvellousdvd@gmail.com>
2025-11-27 09:27:40 +0100

    ready for deployment and demo

--- config/index.tsx
+++ config/index.tsx
@@ -1,6 +1,6 @@
 import { cookieStorage, createStorage, http } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { base, baseSepolia } from '@reown/appkit/networks'
+import { mantle, mantleSepoliaTestnet } from '@reown/appkit/networks'
 
 // Get projectId from https://dashboard.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
@@ -9,7 +9,8 @@ if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [base, baseSepolia]
+// Use Mantle networks
+export const networks = [mantle, mantleSepoliaTestnet]
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
