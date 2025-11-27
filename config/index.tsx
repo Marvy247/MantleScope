23229f596d2ef311e2d69c3b926d2bd459036710
Marvy247 <marvellousdvd@gmail.com>
2025-11-27 10:14:03 +0100

    new commit

--- config/index.tsx
+++ config/index.tsx
@@ -1,25 +1,44 @@
 import { cookieStorage, createStorage, http } from '@wagmi/core'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mantle, mantleSepoliaTestnet } from '@reown/appkit/networks'
+import { createConfig } from 'wagmi'
+import { defineChain } from 'viem'
 
-// Hardcoded Reown project ID
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || 'cb44e6bd7a2139350e8c0fb2d0fea8cb'
+// Define Mantle networks manually
+export const mantle = defineChain({
+  id: 5000,
+  name: 'Mantle',
+  nativeCurrency: { name: 'MNT', symbol: 'MNT', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://rpc.mantle.xyz'] },
+  },
+  blockExplorers: {
+    default: { name: 'Mantle Explorer', url: 'https://explorer.mantle.xyz' },
+  },
+})
 
-if (!projectId) {
-  throw new Error('Project ID is not defined')
-}
+export const mantleSepoliaTestnet = defineChain({
+  id: 5003,
+  name: 'Mantle Sepolia Testnet',
+  nativeCurrency: { name: 'MNT', symbol: 'MNT', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://rpc.sepolia.mantle.xyz'] },
+  },
+  blockExplorers: {
+    default: { name: 'Mantle Sepolia Explorer', url: 'https://explorer.sepolia.mantle.xyz' },
+  },
+  testnet: true,
+})
 
-// Use Mantle networks
 export const networks = [mantle, mantleSepoliaTestnet]
 
-//Set up the Wagmi Adapter (Config)
-export const wagmiAdapter = new WagmiAdapter({
+// Set up the Wagmi Config
+export const config = createConfig({
+  chains: [mantle, mantleSepoliaTestnet],
+  transports: {
+    [mantle.id]: http(),
+    [mantleSepoliaTestnet.id]: http(),
+  },
+  ssr: true,
   storage: createStorage({
     storage: cookieStorage
   }),
-  ssr: true,
-  projectId,
-  networks
 })
-
-export const config = wagmiAdapter.wagmiConfig
